b905d2575a6733f4a4c19b4ebf8c55ecedde45ef
Igor D'Amico Sotolani <contatodmsotolani@gmail.com>
2021-07-28 16:25:32 -0300

    2º Atualização - montando a interface

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
-
-function criarBG() {
-    context.fillStyle = "lightgreen";
-    context.fillRect(0, 0, 16 *box, 16 * box);
+let snake =[];
+snake[0] = {
+    x: 8* box,
+    y: 8* box
 }
 
+function criarBG() {
+    context.fillStyle = "lightblack";
+    context.fillRect(0, 0, 16 *box, 16 * box);
+}
+function criarCobrinha(){
+    for(i=0; i< snake.length; i++){
+        context.fillStyle ="red";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
 criarBG();
+criarCobrinha();
